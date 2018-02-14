ff3922229897cb90c9c11f9cb7ecbac93d70b071
Wesley Sousa Silva <wesley.sousa59@gmail.com>
2018-02-14 13:22:26 -0300

    salvando no banco de dados h2 e formatando data e valor para brasil

--- src/main/java/com/algaworks/cobranca/controller/TituloController.java
+++ src/main/java/com/algaworks/cobranca/controller/TituloController.java
@@ -1,13 +1,29 @@
 package com.algaworks.cobranca.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
-public class TituloController {
+import com.algaworks.cobranca.model.Titulo;
+import com.algaworks.cobranca.repository.Titulos;
 
-	@RequestMapping("/titulos/novo")
+@Controller
+@RequestMapping("/titulos")
+public class TituloController {
+	
+	@Autowired
+	private Titulos titulos;
+
+	@GetMapping("/novo")
 	public String novo() {
+		return "CadastroTitulo";
+	}
+
+	@PostMapping
+	public String salvar(Titulo titulo) {
+		titulos.save(titulo);
 		return "CadastroTitulo";
 	}
 }
