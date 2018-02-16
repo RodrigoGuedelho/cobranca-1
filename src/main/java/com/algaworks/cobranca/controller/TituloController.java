9b611ab27f13fc80f69cc3b23c42b25f9f7f84d5
Wesley Sousa Silva <wesley.sousa59@gmail.com>
2018-02-15 23:37:42 -0300

    criado prototipo de pesquisa de titulos.

--- src/main/java/com/algaworks/cobranca/controller/TituloController.java
+++ src/main/java/com/algaworks/cobranca/controller/TituloController.java
@@ -36,6 +36,11 @@ public class TituloController {
 		return mv;
 	}
 	
+	@GetMapping
+	public String pesquisar(){
+		return "PesquisaTitulos";
+	}
+	
 	@ModelAttribute("todosStatusTitulo")
 	public List<StatusTitulo> todosStatusTitulo(){
 		return Arrays.asList(StatusTitulo.values());
